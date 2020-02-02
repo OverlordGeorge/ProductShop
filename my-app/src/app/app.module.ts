8408eec74bd29d1c382e231f803f7a79d107de08
Unknown <surikhin@rambler.ru>
2020-02-02 18:29:08 +0700

    some changes

---
+++ my-app/src/app/app.module.ts
@@ -0,0 +1,62 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+import { AppComponent } from './app.component';
+import { TopBarComponent } from './top-bar/top-bar.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
+import { CartService } from './cart.service';
+import { CartComponent } from './cart/cart.component';
+import { ShippingComponent } from './shipping/shipping.component';
+import { ShoesDetailsComponent } from './shoes-details/shoes-details.component';
+import { BagsDetailsComponent } from './bags-details/bags-details.component';
+import { PrivacyComponent } from './privacy/privacy.component';
+import { ConditionsComponent } from './conditions/conditions.component';
+import { OrderComponent } from './order/order.component';
+import { OrderService } from './order.service';
+
+
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    RouterModule.forRoot([
+      { path: '', component: ProductListComponent },
+      { path: 'products/:productId', component: ProductDetailsComponent },
+      { path: 'shoes/:shoeId', component: ShoesDetailsComponent},
+      { path: 'bags/:bagId', component: BagsDetailsComponent},
+      { path: 'cart', component: CartComponent },
+      { path: 'shipping', component: ShippingComponent},
+      { path: 'privacy', component: PrivacyComponent},
+      { path: 'conditions', component: ConditionsComponent},
+      { path: 'order', component: OrderComponent},
+    ])
+  ],
+  declarations: [
+    AppComponent,
+    TopBarComponent,
+    ProductListComponent,
+    ProductAlertsComponent,
+    ProductDetailsComponent,
+    ShoesDetailsComponent,
+    BagsDetailsComponent,
+    CartComponent,
+    ShippingComponent,
+    ShoesDetailsComponent,
+    BagsDetailsComponent,
+    PrivacyComponent,
+    ConditionsComponent,
+    OrderComponent,
+  ],
+  bootstrap: [ AppComponent ],
+  providers: [CartService, OrderService]
+})
+export class AppModule { }
+
+
